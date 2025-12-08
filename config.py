c7b4b7c8b20671e9546cd889a0f752c551516d81
pagal4206 <dilipjaat183@gmail.com>
2025-12-08 13:58:16 +0000

    push

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ COOKIE_URL = getenv("COOKIE_URL", "https://pastebin.com/Q0keyBc0")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/adityabty/music")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/pagal4206/newbot")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN")
 
